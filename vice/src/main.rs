977f33be50c4694950fc452408df107a9c9c0f97
ariaandika <andikaakun17@gmail.com>
2025-02-13 14:54:48 +0700

    add sqlx

--- vice/src/main.rs
+++ vice/src/main.rs
@@ -1,19 +1,47 @@
-use axum::{routing::get, Router};
+use anyhow::Context;
+use axum::{extract::State, routing::get, Router};
+use sqlx::postgres::PgPoolOptions;
+use std::{
+    env::var,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+};
 use tokio::net::TcpListener;
 use tracing_subscriber::EnvFilter;
 
-const ADDR: &'static str = "localhost:3000";
+const DEFAULT_HOST: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
+const DEFAULT_PORT: u16 = 3000;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let tcp = TcpListener::bind(ADDR).await?;
-    let routes = Router::new()
-        .route("/", get(||async { "Axum Dev !" }));
+    let tcp = {
+        let addr = match var("ADDR").ok().and_then(|e|e.parse().ok()) {
+            Some(ok) => ok,
+            None => {
+                let host = var("HOST").ok().and_then(|e|e.parse().ok()).unwrap_or(DEFAULT_HOST);
+                let port = var("PORT").ok().and_then(|e|e.parse().ok()).unwrap_or(DEFAULT_PORT);
+                SocketAddr::new(host, port)
+            },
+        };
+        TcpListener::bind(addr).await.with_context(||format!("failed to bind {addr}"))?
+    };
 
-    Ok(axum::serve(tcp,routes).await?)
+    let db = {
+        let db_url = var("DB_URL").context("failed to get DB_URL env")?;
+        PgPoolOptions::new()
+            .connect_lazy(&db_url)
+            .expect("infallible")
+    };
+
+    let routes = Router::new()
+        .route("/", get(||async { "Axum Dev !" }))
+        .with_state(State(db));
+
+    axum::serve(tcp,routes).await.context("failed to serve")
 }
 
