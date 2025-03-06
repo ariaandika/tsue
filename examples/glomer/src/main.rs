3cf44807523e4a887ecf8ebb8f7b0d94f9f4d5c3
ariaandika <andikaakun17@gmail.com>
2025-03-06 12:46:26 +0700

    update example

--- examples/glomer/src/main.rs
+++ examples/glomer/src/main.rs
@@ -1,7 +1,21 @@
+use std::io;
+use vice::{
+    http::{self},
+    router::{get, Router},
+};
 
-
-
-
-fn main() -> anyhow::Result<()> {
-    vice::runtime::listen_blocking("0.0.0.0:3000")
+fn main() -> io::Result<()> {
+    vice::runtime::listen_v2("0.0.0.0:3000", ||Router::new()
+        .route("/", get(index))
+        .route("/foo", get(foo)))
 }
+
+async fn index(_: http::Method, _: http::Method, _: http::Method, body: String) -> String {
+    body
+}
+
+async fn foo(_: http::Method, body: String) -> String {
+    println!("foo");
+    body
+}
+
