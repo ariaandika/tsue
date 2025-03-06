b036e46fbe9c0d1e3f9ac75f2e66d1c50f87b799
ariaandika <andikaakun17@gmail.com>
2025-03-06 15:13:32 +0700

    wrap service with arc internally

--- vice/src/runtime.rs
+++ vice/src/runtime.rs
@@ -3,13 +3,13 @@ use crate::http::{Request, Response};
 use hyper::{server::conn::http1::Builder as Hyper, service::Service};
 use hyper_util::rt::TokioIo;
 use log::error;
-use std::{convert::Infallible, fmt::Display, io, net::ToSocketAddrs};
+use std::{convert::Infallible, fmt::Display, io, net::ToSocketAddrs, sync::Arc};
 use tokio::net::TcpListener;
 
 /// entrypoint to run the server
 pub fn listen<S>(addr: impl ToSocketAddrs + Display + Clone, service: S) -> io::Result<()>
 where
-    S: Service<Request, Response = Response, Error = Infallible> + Clone + Send + 'static,
+    S: Service<Request, Response = Response, Error = Infallible> + Send + Sync + 'static,
     S::Future: Future<Output = Result<Response,Infallible>> + Send + 'static,
 {
     let tcp = std::net::TcpListener::bind(addr.clone()).map_err(|e|tcp_error(addr, e))?;
@@ -20,48 +20,12 @@ where
         .build()?
         .block_on(async move {
             let tcp = TcpListener::from_std(tcp)?;
+            let service = Arc::new(service);
 
             loop {
                 let service = service.clone();
                 match tcp.accept().await {
                     Ok((stream, _)) => {
-                        tokio::spawn(
-                            Hyper::new()
-                                .serve_connection(TokioIo::new(stream), service)
-                                .with_upgrades(),
-                        );
-                    }
-                    Err(err) => {
-                        error!("{err}");
-                    }
-                }
-            }
-        })
-}
-
-/// entrypoint to run the server
-///
-/// alternative to [`listen`], where the service is created every new connection,
-/// no need to share service across threads, thus no clone or arc required
-pub fn listen_v2<F,S>(addr: impl ToSocketAddrs + Display + Clone, service_factory: F) -> io::Result<()>
-where
-    F: Fn() -> S,
-    S: Service<Request, Response = Response, Error = Infallible> + Send + 'static,
-    S::Future: Future<Output = Result<Response,Infallible>> + Send + 'static,
-{
-    let tcp = std::net::TcpListener::bind(addr.clone()).map_err(|e|tcp_error(addr, e))?;
-    tcp.set_nonblocking(true)?;
-
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()?
-        .block_on(async move {
-            let tcp = TcpListener::from_std(tcp)?;
-
-            loop {
-                match tcp.accept().await {
-                    Ok((stream, _)) => {
-                        let service = service_factory();
                         tokio::spawn(
                             Hyper::new()
                                 .serve_connection(TokioIo::new(stream), service)
