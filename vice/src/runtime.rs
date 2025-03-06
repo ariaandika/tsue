bf74ccfb0f85a8d71e7023ec51e247f6ec7dc747
ariaandika <andikaakun17@gmail.com>
2025-03-06 12:40:41 +0700

    create service on demand instad of arc

--- vice/src/runtime.rs
+++ vice/src/runtime.rs
@@ -39,6 +39,43 @@ where
         })
 }
 
+/// entrypoint to run the server
+///
+/// alternative to [`listen`], where the service is created every new connection,
+/// no need to share service across threads, thus no clone or arc required
+pub fn listen_v2<F,S>(addr: impl ToSocketAddrs + Display + Clone, service_factory: F) -> io::Result<()>
+where
+    F: Fn() -> S,
+    S: Service<Request, Response = Response, Error = Infallible> + Clone + Send + 'static,
+    S::Future: Future<Output = Result<Response,Infallible>> + Send + 'static,
+{
+    let tcp = std::net::TcpListener::bind(addr.clone()).map_err(|e|tcp_error(addr, e))?;
+    tcp.set_nonblocking(true)?;
+
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?
+        .block_on(async move {
+            let tcp = TcpListener::from_std(tcp)?;
+
+            loop {
+                match tcp.accept().await {
+                    Ok((stream, _)) => {
+                        let service = service_factory();
+                        tokio::spawn(
+                            Hyper::new()
+                                .serve_connection(TokioIo::new(stream), service)
+                                .with_upgrades(),
+                        );
+                    }
+                    Err(err) => {
+                        error!("{err}");
+                    }
+                }
+            }
+        })
+}
+
 fn tcp_error(addr: impl ToSocketAddrs + Display + Clone, err: io::Error) -> io::Error {
     io::Error::new(err.kind(), format!("failed to bind \"{addr}\" :{err}"))
 }
