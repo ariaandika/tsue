8e1e4768e894b3005f7293f18604d94445efc9aa
ariaandika <andikaakun17@gmail.com>
2025-06-20 09:00:12 +0700

    add default features

--- example/middleware.rs
+++ example/middleware.rs
@@ -1,28 +1,48 @@
 use http::StatusCode;
-use std::io;
+use std::{
+    io,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+};
 use tsue::{
-    request::Request,
+    helper::State,
+    request::{Request, RequestExt},
     response::{IntoResponse, Response},
     routing::{Next, Router, get},
 };
 
+struct App {
+    maintenance: AtomicBool,
+}
+
+type Data = State<Arc<App>>;
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let routes = Router::new()
-        .route("/", get(async || println!("Handler")))
+        .route("/", get(async || "Ok"))
+        .route("/maintenance", get(maintenance))
         .middleware(md)
-    ;
+        .state(Arc::new(App {
+            maintenance: AtomicBool::new(false),
+        }));
 
     tsue::listen("127.0.0.1:3000", routes).await
 }
 
-async fn md(req: Request, next: Next) -> Response {
-    println!("[PRE]");
-    if req.uri().path() == "/foo" {
-        return StatusCode::NOT_FOUND.into_response();
+async fn maintenance(state: Data) {
+    state.maintenance.fetch_not(Ordering::Relaxed);
+}
+
+async fn md(mut req: Request, next: Next) -> Response {
+    let app = req.extract_parts::<Data>().await.unwrap();
+
+    if req.uri().path() != "/maintenance" && app.maintenance.load(Ordering::Relaxed) {
+        return (StatusCode::SERVICE_UNAVAILABLE, "Under Maintenance").into_response();
     }
-    let res = next.next(req).await;
-    println!("[POST]");
-    res
+
+    next.next(req).await
 }
 
