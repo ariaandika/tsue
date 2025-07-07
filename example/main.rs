390faa5cb084585d6c9039cacb2a40b4eb15f2e5
ariaandika <andikaakun17@gmail.com>
2025-07-07 22:22:22 +0700

    update example

--- example/main.rs
+++ example/main.rs
@@ -1,6 +1,12 @@
 use std::io;
+use tcio::fmt::lossy;
 use tokio::{net::TcpListener, runtime::Runtime};
-use tsue::{request::Request, service::from_fn};
+use tsue::{
+    body::Body,
+    request::Request,
+    response::{Parts, Response},
+    service::from_fn,
+};
 
 fn main() -> io::Result<()> {
     Runtime::new()
@@ -9,12 +15,19 @@ fn main() -> io::Result<()> {
             let io = TcpListener::bind("0.0.0.0:3000").await?;
 
             tsue::rt::serve(io, from_fn(handle)).await;
-
             Ok(())
         })
 }
 
-async fn handle(_req: Request) {
-    
+async fn handle(req: Request) -> Response {
+    let parts = req.parts();
+    println!("> {} {} {}", parts.method, parts.uri, parts.version);
+
+    if parts.uri.path() != "/null" {
+        let body = req.into_body().collect().await.unwrap();
+        println!("{}",lossy(&body));
+    }
+
+    Response::from_parts(Parts::default(), Body::new(&b"Hell"[..]))
 }
 
