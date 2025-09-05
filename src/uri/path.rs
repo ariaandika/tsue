b40966024564c1325d7d54a32e5155f786a7a5cf
ariaandika <andikaakun17@gmail.com>
2025-09-05 23:39:42 +0700

    add validation for path struct

--- src/uri/path.rs
+++ src/uri/path.rs
@@ -1,14 +1,68 @@
 use tcio::bytes::Bytes;
 
+use super::{simd, UriError};
+
 #[derive(Clone)]
 pub struct Path {
+    /// is valid ASCII
     value: Bytes,
     query: u16,
 }
 
 impl Path {
-    pub fn from_shared(value: Bytes) -> Self {
-        todo!()
+    pub fn from_shared(mut value: Bytes) -> Self {
+        let query = simd::match_query! {
+            value;
+            |val, cursor| match val {
+                b'?' => {
+                    let query = cursor.steps();
+                    simd::match_fragment! {
+                        cursor;
+                        |val| match val {
+                            b'#' => cursor.truncate_buf(),
+                            _ => UriError::Char.panic_const()
+                        };
+                    }
+                    query
+                },
+                b'#' => {
+                    cursor.truncate_buf();
+                    value.len()
+                }
+                _ => UriError::Char.panic_const()
+            };
+            else {
+                value.len()
+            }
+        };
+        Self {
+            value,
+            query: query as u16,
+        }
+    }
+
+    /// Returns the path as `str`, e.g: `/over/there`.
+    #[inline]
+    pub fn path(&self) -> &str {
+        // SAFETY: precondition `value` is valid ASCII
+        unsafe { str::from_utf8_unchecked(&self.value[..self.query as usize]) }
+    }
+
+    /// Returns the query as `str`, e.g: `name=joe&query=4`.
+    #[inline]
+    pub fn query(&self) -> Option<&str> {
+        if self.query as usize == self.value.len() {
+            None
+        } else {
+            // SAFETY: precondition `value` is valid ASCII
+            unsafe { Some(str::from_utf8_unchecked(&self.value[self.query as usize + 1..])) }
+        }
+    }
+
+    /// Returns the path and query as `str`, e.g: `/over/there?name=joe&query=4`.
+    #[inline]
+    pub const fn path_and_query(&self) -> &str {
+        self.as_str()
     }
 
     #[inline]
--- src/uri/simd.rs
+++ src/uri/simd.rs
@@ -119,7 +119,11 @@ macro_rules! validate_authority {
 }
 
 macro_rules! match_query {
-    ($cursor:ident) => {
+    (
+        $bytes:expr;
+        |$val:ident,$cursor:ident|$matches:expr;
+        else { $el:expr }
+    ) => {
         'swar: {
             const BLOCK: usize = size_of::<usize>();
             const MSB: usize = usize::from_ne_bytes([0b1000_0000; BLOCK]);
@@ -128,14 +132,13 @@ macro_rules! match_query {
             const BANG: usize = usize::from_ne_bytes([b'!'; BLOCK]);
             const QS: usize = usize::from_ne_bytes([b'?'; BLOCK]);
             const HASH: usize = usize::from_ne_bytes([b'#'; BLOCK]);
-            const SLASH: usize = usize::from_ne_bytes([b'/'; BLOCK]);
             const DEL: usize = usize::from_ne_bytes([127; BLOCK]);
+
+            let mut $cursor = $bytes.cursor_mut();
 
             while let Some(chunk) = $cursor.peek_chunk::<BLOCK>() {
                 let block = usize::from_ne_bytes(*chunk);
 
-                // '/'
-                let is_sl = (block ^ SLASH).wrapping_sub(LSB);
                 // '?'
                 let is_qs = (block ^ QS).wrapping_sub(LSB);
                 // '#'
@@ -145,30 +148,86 @@ macro_rules! match_query {
                 // 127(DEL)
                 let is_del = (block ^ DEL).wrapping_sub(LSB);
 
-                let result = (is_sl | is_qs | is_hs | lt_33 | is_del | block) & MSB;
+                let result = (is_qs | is_hs | lt_33 | is_del | block) & MSB;
                 if result != 0 {
-                    $cursor.advance((result.trailing_zeros() / 8) as usize);
-                    break 'swar;
+                    let nth = (result.trailing_zeros() / 8) as usize;
+                    $cursor.advance(nth);
+                    let $val = chunk[nth];
+                    break 'swar $matches;
                 }
 
                 $cursor.advance(BLOCK);
             }
 
-            while let Some(byte) = $cursor.next() {
+            while let Some($val) = $cursor.next() {
                 simd::byte_map! {
                     const PAT =
                         #[default(true)]
                         #[false](b'!'..=b'~')
-                        #[true](b'/' | b'?' | b'#')
+                        #[true](b'?' | b'#')
                 }
 
-                if PAT[byte as usize] {
+                if PAT[$val as usize] {
                     $cursor.step_back(1);
-                    break 'swar;
+                    break 'swar $matches;
+                }
+            }
+
+            $el
+        }
+    };
+}
+
+macro_rules! match_fragment {
+    (
+        $cursor:expr;
+        |$val:ident|$matches:expr;
+    ) => {
+        'swar: {
+            const BLOCK: usize = size_of::<usize>();
+            const MSB: usize = usize::from_ne_bytes([0b1000_0000; BLOCK]);
+            const LSB: usize = usize::from_ne_bytes([0b0000_0001; BLOCK]);
+
+            const BANG: usize = usize::from_ne_bytes([b'!'; BLOCK]);
+            const HASH: usize = usize::from_ne_bytes([b'#'; BLOCK]);
+            const DEL: usize = usize::from_ne_bytes([127; BLOCK]);
+
+            while let Some(chunk) = $cursor.peek_chunk::<BLOCK>() {
+                let block = usize::from_ne_bytes(*chunk);
+
+                // '#'
+                let is_hs = (block ^ HASH).wrapping_sub(LSB);
+                // 33('!') < byte
+                let lt_33 = block.wrapping_sub(BANG);
+                // 127(DEL)
+                let is_del = (block ^ DEL).wrapping_sub(LSB);
+
+                let result = (is_hs | lt_33 | is_del | block) & MSB;
+                if result != 0 {
+                    let nth = (result.trailing_zeros() / 8) as usize;
+                    $cursor.advance(nth);
+                    let $val = chunk[nth];
+                    break 'swar $matches;
+                }
+
+                $cursor.advance(BLOCK);
+            }
+
+            while let Some($val) = $cursor.next() {
+                simd::byte_map! {
+                    const PAT =
+                        #[default(true)]
+                        #[false](b'!'..=b'~')
+                        #[true](b'#')
+                }
+
+                if PAT[$val as usize] {
+                    $cursor.step_back(1);
+                    break 'swar $matches;
                 }
             }
         }
     };
 }
 
-pub(crate) use {byte_map, validate_scheme, validate_authority};
+pub(crate) use {byte_map, validate_scheme, validate_authority, match_query, match_fragment};
