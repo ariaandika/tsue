b45681f2807f19fad5e8d7c529c2680d06e89807
ariaandika <andikaakun17@gmail.com>
2025-11-10 23:46:29 +0700

    add internal docs for h1 module

--- src/h1/mod.rs
+++ src/h1/mod.rs
@@ -4,8 +4,20 @@
 //! - [`proto`] contains HTTP/1.1 related logic.
 //! - [`io`] contains IO related APIs
 //! - [`driver`] contains the integration of all the components above into single API
+
+/// Usage
+///
+/// [`parser`], parse http request line, request target, and headers
+///
+/// [`io`], encoding aware io reading and streaming
+///
+/// [`proto`], semantically build request object
+mod doc {}
+
 pub mod parser;
-pub mod proto;
 pub mod io;
+
+pub mod proto;
 pub mod driver;
+
 pub mod error;
