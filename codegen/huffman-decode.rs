7e79e8a9df87f6a8cb3da97379e69a1667268806
ariaandika <andikaakun17@gmail.com>
2026-01-28 19:40:19 +0700

    merge decode modules codegen

--- codegen/huffman-decode.rs
+++ codegen/huffman-decode.rs
@@ -2,7 +2,6 @@
 //
 // in the future it will be interesting to explore different approaches
 use huffman::{source_iter, Bits};
-use decode::{DecodeEntry, EntryData};
 
 mod huffman;
 mod decode;
@@ -234,4 +233,68 @@ fn print_decode_pretty(decode: DecodeTable) {
     }
 
     println!("];");
+}
+
+// ===== State =====
+
+#[derive(Clone, Debug)]
+pub enum DecodeEntry {
+    None,
+    Some {
+        next: usize,
+        maybe_eos: bool,
+        shifts: usize,
+    },
+    Decoded {
+        next: usize,
+        maybe_eos: bool,
+        shifts: usize,
+        byte: u8,
+        tagged_bit_mask: u8,
+    },
+    #[allow(dead_code)]
+    Error,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct EntryData {
+    pub byte: u8,
+    pub shifts: usize,
+}
+
+impl DecodeEntry {
+    pub const fn none() -> Self {
+        Self::None
+    }
+
+    pub fn some(data: EntryData, maybe_eos: bool, next: usize) -> Self {
+        Self::Some {
+            next,
+            maybe_eos,
+            shifts: data.shifts,
+        }
+    }
+
+    pub const fn decoded(data: EntryData, maybe_eos: bool, next: usize, tagged_bit_mask: u8) -> Self {
+        Self::Decoded {
+            byte: data.byte,
+            shifts: data.shifts,
+            maybe_eos,
+            next,
+            tagged_bit_mask,
+        }
+    }
+
+    pub const fn new_entries() -> [Self; 16] {
+        [const { Self::none() }; 16]
+    }
+
+    /// Current entry must be `None`, otherwise panic.
+    pub fn set(&mut self, me: Self) {
+        assert!(
+            matches!(self, Self::None),
+            "duplicate assignment on DecodeEntry: {self:?} for {me:?}"
+        );
+        *self = me;
+    }
 }
