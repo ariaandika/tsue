2681bcef1b9070d4e11d25ef43fb3d0569a5ba72
ariaandika <andikaakun17@gmail.com>
2026-02-07 19:01:41 +0700

    flatten HpackError variant

--- src/h2/hpack/error.rs
+++ src/h2/hpack/error.rs
@@ -3,26 +3,26 @@ use crate::h2::hpack::huffman::HuffmanError;
 use crate::headers::error::{HeaderError, TryReserveError};
 
 /// HPACK Related Error.
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum HpackError {
     /// Bytes is insufficient.
     Incomplete,
+    /// Bytes is too large.
+    TooLarge,
     /// Header fields is too many.
     TooMany,
-    /// Unknown header block kind.
-    UnknownRepr,
-    /// invalid use of index 0.
+    /// Invalid use of index 0.
     ZeroIndex,
     /// Indexed header not found.
     NotFound,
     /// Huffman coding error.
     Huffman,
+    /// Header validation error.
+    InvalidHeader,
     /// Pseudo header is not at the beginning of header block.
     InvalidPseudoHeader,
     /// Size update is too large or is not at the beginning of header block.
     InvalidSizeUpdate,
-    /// Header name or value validation error.
-    InvalidHeader(HeaderError),
 }
 
 impl std::error::Error for HpackError { }
@@ -30,12 +30,12 @@ impl std::fmt::Display for HpackError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Incomplete => f.write_str("bytes incomplete"),
+            Self::TooLarge => f.write_str("bytes too large"),
             Self::TooMany => f.write_str("header too many"),
-            Self::UnknownRepr => f.write_str("unknown header field representation"),
             Self::ZeroIndex => f.write_str("invalid use of index 0"),
             Self::NotFound => f.write_str("indexed header not found"),
             Self::Huffman => f.write_str("huffman coding error"),
-            Self::InvalidHeader(err) => write!(f, "invalid header: {err}"),
+            Self::InvalidHeader => f.write_str("invalid header"),
             Self::InvalidPseudoHeader => f.write_str("missplaced pseudo header"),
             Self::InvalidSizeUpdate => f.write_str("invalid size update"),
         }
@@ -43,18 +43,25 @@ impl std::fmt::Display for HpackError {
 }
 
 impl From<HeaderError> for HpackError {
+    #[inline]
     fn from(err: HeaderError) -> Self {
-        Self::InvalidHeader(err)
+        match err {
+            HeaderError::Empty => Self::Incomplete,
+            HeaderError::TooLong => Self::TooLarge,
+            HeaderError::Invalid => Self::InvalidHeader,
+        }
     }
 }
 
 impl From<HuffmanError> for HpackError {
+    #[inline]
     fn from(_: HuffmanError) -> Self {
         Self::Huffman
     }
 }
 
 impl From<TryReserveError> for HpackError {
+    #[inline]
     fn from(_: TryReserveError) -> Self {
         Self::TooMany
     }
