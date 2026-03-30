7e545af07b804127150b9b377e329ba602f19def
ariaandika <andikaakun17@gmail.com>
2026-03-30 19:10:41 +0700

    add missing status code constants

--- src/http/status.rs
+++ src/http/status.rs
@@ -1,8 +1,18 @@
 use std::num::NonZeroU16;
 
-/// HTTP [Status Code][rfc].
+/// HTTP Status Code.
 ///
-/// [rfc]: <https://datatracker.ietf.org/doc/html/rfc9110#name-status-codes>
+/// Status code is a three-digit integer code in a response that describes the result of the
+/// request and the semantics of the response, including whether the request was successful and
+/// what content is enclosed (if any).
+///
+/// This API supports status codes defined in [RFC9110] and [RFC6585], [451 (Unavailable For Legal
+/// Reasons)][RFC7725], and [103 (Early Hints)][RFC8297]
+///
+/// [RFC9110]: <https://www.rfc-editor.org/rfc/rfc9110#name-status-codes>
+/// [RFC6585]: <https://www.rfc-editor.org/rfc/rfc6585>
+/// [RFC7725]: <https://www.rfc-editor.org/rfc/rfc7725>
+/// [RFC8297]: <https://www.rfc-editor.org/rfc/rfc8297>
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct StatusCode(NonZeroU16);
 
@@ -118,102 +128,183 @@ macro_rules! status_code_v3 {
 }
 
 status_code_v3! {
-    /// `101`, This code is sent in response to an `Upgrade` request header from the client and indicates
-    /// the protocol the server is switching to.
+    /// The 100 (Continue) status code indicates that the initial part of a request has been
+    /// received and has not yet been rejected by the server. The server intends to send a final
+    /// response after the request has been fully received and acted upon.
+    100 CONTINUE "Continue";
+    /// The 101 (Switching Protocols) status code indicates that the server understands and is
+    /// willing to comply with the client's request, via the Upgrade header field, for a change in
+    /// the application protocol being used on this connection.
     101 SWITCHING_PROTOCOL "Switching Protocols";
-    /// `200`. The request succeeded.
+    /// The 103 (Early Hints) informational status code indicates to the client that the server is
+    /// likely to send a final response with the header fields included in the informational
+    /// response.
+    103 EARLY_HINTS "Early Hints";
+    /// The 200 (OK) status code indicates that the request has succeeded.
     200 OK "OK";
-    /// `201`. The request succeeded, and a new resource was created as a result.
+    /// The 201 (Created) status code indicates that the request has been fulfilled and has
+    /// resulted in one or more new resources being created.
     201 CREATED "Created";
-    /// `204`. There is no content to send for this request, but the headers are useful.
+    /// The 202 (Accepted) status code indicates that the request has been accepted for processing,
+    /// but the processing has not been completed.
+    202 ACCEPTED "Accepted";
+    /// The 203 (Non-Authoritative Information) status code indicates that the request was
+    /// successful but the enclosed content has been modified from that of the origin server's 200
+    /// (OK) response by a transforming proxy.
+    203 NON_AUTHORATIVE_INFORMATION "Non-Authoritative Information";
+    /// The 204 (No Content) status code indicates that the server has successfully fulfilled the
+    /// request and that there is no additional content to send in the response content.
     204 NO_CONTENT "No Content";
-    /// `302`. This response code means that the URI of requested resource has been changed temporarily.
+    /// The 205 (Reset Content) status code indicates that the server has fulfilled the request and
+    /// desires that the user agent reset the "document view", which caused the request to be sent,
+    /// to its original state as received from the origin server.
+    205 RESET_CONTENT "Reset Content";
+    /// The 206 (Partial Content) status code indicates that the server is successfully fulfilling
+    /// a range request for the target resource by transferring one or more parts of the selected
+    /// representation.
+    206 PARTIAL_CONTENT "Partial Content";
+    /// The 300 (Multiple Choices) status code indicates that the target resource has more than one
+    /// representation, each with its own more specific identifier, and information about the
+    /// alternatives is being provided so that the user (or user agent) can select a preferred
+    /// representation by redirecting its request to one or more of those identifiers.
+    300 MULTIPLE_CHOICES "Multiple Choices";
+    /// The 301 (Moved Permanently) status code indicates that the target resource has been
+    /// assigned a new permanent URI and any future references to this resource ought to use one of
+    /// the enclosed URIs.
+    301 MOVED_PERMANENTLY "Moved Permanently";
+    /// The 302 (Found) status code indicates that the target resource resides temporarily under a
+    /// different URI.
     302 FOUND "Found";
-    /// `303`. The server sent this response to direct the client to get the requested resource at another
-    /// URI with a GET request.
+    /// The 303 (See Other) status code indicates that the server is redirecting the user agent to
+    /// a different resource, as indicated by a URI in the Location header field, which is intended
+    /// to provide an indirect response to the original request
     303 SEE_OTHER "See Other";
-    /// `304`. This is used for caching purposes. It tells the client that the response has not been
-    /// modified, so the client can continue to use the same cached version of the response.
+    /// The 304 (Not Modified) status code indicates that a conditional GET or HEAD request has
+    /// been received and would have resulted in a 200 (OK) response if it were not for the fact
+    /// that the condition evaluated to false.
     304 NOT_MODIFIED "Not Modified";
-    /// `307`. The server sends this response to direct the client to get the requested resource at
-    /// another URI with the same method that was used in the prior request.
+    /// The 307 (Temporary Redirect) status code indicates that the target resource resides
+    /// temporarily under a different URI and the user agent MUST NOT change the request method if
+    /// it performs an automatic redirection to that URI.
     307 TEMPORARY_REDIRECT "Temporary Redirect";
-    /// `400`. The server cannot or will not process the request due to something that is perceived to be
-    /// a client error.
+    /// The 308 (Permanent Redirect) status code indicates that the target resource has been
+    /// assigned a new permanent URI and any future references to this resource ought to use one of
+    /// the enclosed URIs.
+    308 PERMANENT_REDIRECT "Permanent Redirect";
+    /// The 400 (Bad Request) status code indicates that the server cannot or will not process the
+    /// request due to something that is perceived to be a client error (e.g., malformed request
+    /// syntax, invalid request message framing, or deceptive request routing).
     400 BAD_REQUEST "Bad Request";
-    /// `401`. Although the HTTP standard specifies "unauthorized", semantically this response means
-    /// "unauthenticated".
+    /// The 401 (Unauthorized) status code indicates that the request has not been applied because
+    /// it lacks valid authentication credentials for the target resource.
     401 UNAUTHORIZED "Unauthorized";
-    /// `403`. The client's identity is known to the server, but client does not have access rights
-    /// to the content.
+    // The 402 (Payment Required) status code is reserved for future use.
+    /// The 403 (Forbidden) status code indicates that the server understood the request but
+    /// refuses to fulfill it.
     403 FORBIDDEN "Forbidden";
-    /// `404`. The server cannot find the requested resource.
+    /// The 404 (Not Found) status code indicates that the origin server did not find a current
+    /// representation for the target resource or is not willing to disclose that one exists.
     404 NOT_FOUND "Not Found";
-    /// `405`. The request method is known by the server but is not supported by the target resource.
+    /// The 405 (Method Not Allowed) status code indicates that the method received in the
+    /// request-line is known by the origin server but not supported by the target resource.
     405 METHOD_NOT_ALLOWED "Method Not Allowed";
-    /// `406`. This response is sent when the web server, after performing server-driven content
-    /// negotiation, doesn't find any content that conforms to the criteria given by the user
-    /// agent.
+    /// The 406 (Not Acceptable) status code indicates that the target resource does not have a
+    /// current representation that would be acceptable to the user agent, according to the
+    /// proactive negotiation header fields received in the request, and the server is unwilling to
+    /// supply a default representation.
     406 NOT_ACCEPTABLE "Not Acceptable";
-    /// `408`. This response is sent on an idle connection by some servers, even without any previous
-    /// request by the client. It means that the server would like to shut down this unused
-    /// connection.
+    /// The 407 (Proxy Authentication Required) status code is similar to 401 (Unauthorized), but
+    /// it indicates that the client needs to authenticate itself in order to use a proxy for this
+    /// request.
+    407 PROXY_AUTHENTICATION_REQUIRED "Proxy Authentication Required";
+    /// The 408 (Request Timeout) status code indicates that the server did not receive a complete
+    /// request message within the time that it was prepared to wait.
     408 REQUEST_TIMEOUT "Request Timeout";
-    /// `411`. Server rejected the request because the `Content-Length` header field is not defined and the
-    /// server requires it.
+    /// The 409 (Conflict) status code indicates that the request could not be completed due to a
+    /// conflict with the current state of the target resource.
+    409 CONFLICT "Conflict";
+    /// The 410 (Gone) status code indicates that access to the target resource is no longer
+    /// available at the origin server and that this condition is likely to be permanent.
+    410 GONE "Gone";
+    /// The 411 (Length Required) status code indicates that the server refuses to accept the
+    /// request without a defined Content-Length.
     411 LENGTH_REQUIRED "Length Required";
-    /// `412`. In conditional requests, the client has indicated preconditions in its headers which the
-    /// server does not meet.
+    /// The 412 (Precondition Failed) status code indicates that one or more conditions given in
+    /// the request header fields evaluated to false when tested on the server.
     412 PRECONDITION_FAILED "Precondition Failed";
-    /// `413`. The request body is larger than limits defined by server. The server might close the
-    /// connection or return an `Retry-After` header field.
+    /// The 413 (Content Too Large) status code indicates that the server is refusing to process a
+    /// request because the request content is larger than the server is willing or able to
+    /// process.
     413 CONTENT_TOO_LARGE "Content Too Large";
-    /// `414`. The URI requested by the client is longer than the server is willing to interpret.
+    /// The 414 (URI Too Long) status code indicates that the server is refusing to service the
+    /// request because the target URI is longer than the server is willing to interpret.
     414 URI_TOO_LONG "URI Too Long";
-    /// `415`. The media format of the requested data is not supported by the server, so the server is
-    /// rejecting the request.
+    /// The 415 (Unsupported Media Type) status code indicates that the origin server is refusing
+    /// to service the request because the content is in a format not supported by this method on
+    /// the target resource.
     415 UNSUPPORTED_MEDIA_TYPE "Unsupported Media Type";
-    /// `416`. The ranges specified by the `Range` header field in the request cannot be fulfilled. It's
-    /// possible that the range is outside the size of the target resource's data.
+    /// The 416 (Range Not Satisfiable) status code indicates that the set of ranges in the
+    /// request's Range header field has been rejected either because none of the requested ranges
+    /// are satisfiable or because the client has requested an excessive number of small or
+    /// overlapping ranges (a potential denial of service attack).
     416 RANGE_NOT_SATISFIABLE "Range Not Satisfiable";
-    /// `417`. This response code means the expectation indicated by the `Expect` request header field
-    /// cannot be met by the server.
+    /// The 417 (Expectation Failed) status code indicates that the expectation given in the
+    /// request's Expect header field could not be met by at least one of the inbound servers.
     417 EXPECTATION_FAILED "Expectation Failed";
-    /// `418`. The server refuses the attempt to brew coffee with a teapot.
+    /// The 418 (I'm a teapot) status code indicates that the server refuses the attempt to brew
+    /// coffee with a teapot.
     418 IM_A_TEAPOT "I'm a teapot";
-    /// `429`. The user has sent too many requests in a given amount of time ([rate limiting][1]).
-    ///
-    /// [1]: <https://developer.mozilla.org/en-US/docs/Glossary/Rate_limit>
+    /// The 421 (Misdirected Request) status code indicates that the request was directed at a
+    /// server that is unable or unwilling to produce an authoritative response for the target URI.
+    421 MISDIRECTED_REQUEST "Misdirected Request";
+    /// The 422 (Unprocessable Content) status code indicates that the server understands the
+    /// content type of the request content (hence a 415 (Unsupported Media Type) status code is
+    /// inappropriate), and the syntax of the request content is correct, but it was unable to
+    /// process the contained instructions.
+    422 UNPROCESSABLE_CONTENT "Unprocessable Content";
+    /// The 426 (Upgrade Required) status code indicates that the server refuses to perform the
+    /// request using the current protocol but might be willing to do so after the client upgrades
+    /// to a different protocol.
+    426 UPGRADE_REQUIRED "Upgrade Required";
+    /// The 428 (Precondition Required) status code indicates that the origin server requires the
+    /// request to be conditional.
+    428 PRECONDITION_REQUIRED "Precondition Required";
+    /// The 429 (Too Many Requests) status code indicates that the user has sent too many requests
+    /// in a given amount of time ("rate limiting").
     429 TOO_MANY_REQUESTS "Too Many Requests";
-    /// `431`. The server is unwilling to process the request because its header fields are too large. The
-    /// request may be resubmitted after reducing the size of the request header fields.
+    /// The 431 (Request Header Fields Too Large) status code indicates that the server is
+    /// unwilling to process the request because its header fields are too large.
     431 REQUEST_HEADER_FIELDS_TOO_LARGE "Request Header Fields Too Large";
-    /// `500`. The server has encountered a situation it does not know how to handle. This error is
-    /// generic, indicating that the server cannot find a more appropriate 5XX status code to
-    /// respond with.
+    /// The 500 (Internal Server Error) status code indicates that the server encountered an
+    /// unexpected condition that prevented it from fulfilling the request.
     500 INTERNAL_SERVER_ERROR "Internal Server Error";
-    /// `501`. The request method is not supported by the server and cannot be handled. The only methods
-    /// that servers are required to support (and therefore that must not return this code) are GET
-    /// and HEAD.
+    /// The 501 (Not Implemented) status code indicates that the server does not support the
+    /// functionality required to fulfill the request.
     501 NOT_IMPLEMENTED "Not Implemented";
-    /// `502`. This error response means that the server, while working as a gateway to get a response
-    /// needed to handle the request, got an invalid response.
+    /// The 502 (Bad Gateway) status code indicates that the server, while acting as a gateway or
+    /// proxy, received an invalid response from an inbound server it accessed while attempting to
+    /// fulfill the request.
     502 BAD_GATEWAY "Bad Gateway";
-    /// `503`. The server is not ready to handle the request.
-    ///
-    /// Common causes are a server that is down for maintenance or that is overloaded. Note that
-    /// together with this response, a user-friendly page explaining the problem should be sent.
+    /// The 503 (Service Unavailable) status code indicates that the server is currently unable to
+    /// handle the request due to a temporary overload or scheduled maintenance, which will likely
+    /// be alleviated after some delay.
     503 SERVICE_UNAVAILABLE "Service Unavailable";
-    /// `504`. This error response is given when the server is acting as a gateway and cannot get a
-    /// response in time.
+    /// The 504 (Gateway Timeout) status code indicates that the server, while acting as a gateway
+    /// or proxy, did not receive a timely response from an upstream server it needed to access in
+    /// order to complete the request.
     504 GATEWAY_TIMEOUT "Gateway Timeout";
-    /// `505`. The HTTP version used in the request is not supported by the server.
+    /// The 505 (HTTP Version Not Supported) status code indicates that the server does not
+    /// support, or refuses to support, the major version of HTTP that was used in the request
+    /// message.
     505 HTTP_VERSION_NOT_SUPPORTED "HTTP Version Not Supported";
+    /// The 511 (Network Authentication Required) status code indicates that the client needs to
+    /// authenticate to gain network access.
+    511 NETWORK_AUTHENTICATION_REQUIRED "Network Authentication Required";
 }
 
 impl std::fmt::Display for StatusCode {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.write_str(self.as_str())
+        self.status_str().fmt(f)
     }
 }
 
